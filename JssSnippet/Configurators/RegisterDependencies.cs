c7c79f45ee1c8917826b6e3b544c9cd8e9d290a7
Corey Smith <coreyas@gmail.com>
2021-09-16 09:44:35 -0400

    Simplify service registration

--- JssSnippet/Configurators/RegisterDependencies.cs
+++ JssSnippet/Configurators/RegisterDependencies.cs
@@ -1,12 +1,8 @@
 ﻿using JssSnippet.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Sitecore.DependencyInjection;
-using Sitecore.LayoutService.Configuration;
 using Sitecore.LayoutService.ItemRendering;
-using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
-using Sitecore.LayoutService.Presentation.Pipelines.RenderJsonRendering;
-using Sitecore.LayoutService.Serialization;
-using Sitecore.LayoutService.Serialization.Pipelines.GetFieldSerializer;
 
 namespace JssSnippet.Configurators
 {
@@ -14,15 +10,7 @@ namespace JssSnippet.Configurators
     {
         public void Configure(IServiceCollection serviceCollection)
         {
-            serviceCollection.AddSingleton<IConfiguration, Configuration>();
-            serviceCollection.AddSingleton<IFieldRenderer, FieldRenderer>();
-            serviceCollection.AddSingleton<ISerializerService, SerializerService>();
-            serviceCollection.AddSingleton<IRenderJsonRenderingPipeline, RenderJsonRenderingPipeline>();
-            serviceCollection.AddSingleton<IGetFieldSerializerPipeline, GetFieldSerializerPipeline>();
-            serviceCollection.AddSingleton<IPlaceholderRenderingService, PlaceholderRenderingService>();
-            serviceCollection.AddSingleton<ILayoutServiceContext, PipelineLayoutServiceContext>();
-            serviceCollection.AddSingleton<IGetLayoutServiceContextPipeline, GetLayoutServiceContextPipeline>();
-            serviceCollection.AddSingleton<ILayoutService, CustomLayoutService>();
+            serviceCollection.Replace(ServiceDescriptor.Singleton<ILayoutService, CustomLayoutService>());
         }
     }
 }
